1468502152bfdc48f520374856f933055a4f07f3
Golan Shany <golans@cs.huji.ac.il>
2021-11-15 10:53:22 +0200

    Update sol3.py

--- sol3.py
+++ sol3.py
@@ -29,12 +29,13 @@ def create_filter_vec(filter_size):
     """
     filter_vec = np.array([1, 1])
     final_filter_vec = np.array([1, 1])
-    for i in range(3, filter_size + 1, 2):
-        final_filter_vec = signal.convolve(in1=final_filter_vec, in2=filter_vec)
+    for i in range(3, filter_size + 1):
+        final_filter_vec = \
+            signal.convolve(in1=final_filter_vec, in2=filter_vec)
     # normalize
     final_filter_vec = final_filter_vec / final_filter_vec.sum()
     # print(f"final_filter_vec: {final_filter_vec}")
-    return final_filter_vec
+    return final_filter_vec.reshape(1, final_filter_vec.shape[0])
 
 
 def reduce(image, filter_vec):
@@ -53,11 +54,11 @@ def reduce(image, filter_vec):
     """
     # blur
     # as a row vector
-    row_filter_vec = filter_vec.reshape(filter_vec.shape[0], 1)
+    row_filter_vec = filter_vec
     reduced_image = ndimage.filters.convolve(input=image, weights=row_filter_vec, output=None, mode='reflect', cval=0.0,
                                              origin=0)
     # as a column vector
-    col_filter_vec = filter_vec.reshape(1, filter_vec.shape[0])
+    col_filter_vec = filter_vec.reshape(filter_vec.shape[1], 1)
     reduced_image = ndimage.filters.convolve(input=reduced_image, weights=col_filter_vec, output=None, mode='reflect',
                                              cval=0.0, origin=0)
     # sub-sample
@@ -122,14 +123,16 @@ def build_gaussian_pyramid(im, max_levels, filter_size):
     """
     pyr = []  # output is a python list
     filter_vec = create_filter_vec(filter_size)
-    num_rows = len(im)
-    num_cols = len(im[0])
     new_im = im
+    num_rows = len(new_im)
+    num_cols = len(new_im[0])
     while not end_of_pyramid(pyr, max_levels, num_rows, num_cols):
         # insert a level to the top of the pyramid
         pyr.insert(len(pyr), new_im)
-        if not end_of_pyramid(pyr, max_levels, num_rows, num_cols):
-            new_im = reduce(new_im, filter_vec)
+        # if not end_of_pyramid(pyr, max_levels, num_rows, num_cols):
+        new_im = reduce(new_im, filter_vec)
+        num_rows = len(new_im)
+        num_cols = len(new_im[0])
     return pyr, filter_vec
 
 
@@ -147,19 +150,29 @@ def expand(image, filter_vec):
     # blur
     # to maintain constant brightness 2*filter should actually be used
     # as a row vector
-    row_filter_vec = filter_vec.reshape(filter_vec.shape[0], 1)
-    expanded_image = ndimage.filters.convolve(
+    row_filter_vec = filter_vec
+    temp = ndimage.filters.convolve(
         input=image, weights=2*row_filter_vec, output=None, mode='reflect',
         cval=0.0, origin=0)
     # as a column vector
-    col_filter_vec = filter_vec.reshape(1, filter_vec.shape[0])
-    expanded_image = ndimage.filters.convolve(
-        input=expanded_image, weights=2*col_filter_vec, output=None,
+    col_filter_vec = filter_vec.reshape(filter_vec.shape[1], 1)
+    temp = ndimage.filters.convolve(
+        input=temp, weights=2*col_filter_vec, output=None,
         mode='reflect', cval=0.0, origin=0)
-    # up-sample by adding zeros in the odd places
-    expanded_image = np.dstack(
-        (expanded_image,np.zeros_like(expanded_image))).reshape(
-        expanded_image.shape[0],-1)
+    # up-sample by adding zeros in the odd places.
+    # for rows
+    # expanded_image = np.dstack(
+    #    (expanded_image, np.zeros_like(expanded_image))).reshape(
+    #    expanded_image.shape[0], -1)
+    # for rows
+    # expanded_image = np.hstack(
+    #     (expanded_image, np.zeros_like(expanded_image[0]))).reshape(
+    #     -1, expanded_image.shape[1])
+    expanded_image = np.zeros((2*image.shape[0], 2*image.shape[1]))
+    expanded_image[::2, ::2] = temp
+    # todo: should remove last ow and col? im len is a power of 2
+    # expanded_image = np.delete(expanded_image, -1, 0)
+    # expanded_image = np.delete(expanded_image, -1, 1)
     return expanded_image
 
 
@@ -202,8 +215,6 @@ def build_laplacian_pyramid(im, max_levels, filter_size):
     filter_vec should be normalized.
     """
     pyr = []  # output is a python list
-    num_rows = len(im)
-    num_cols = len(im[0])
     gaussian_pyramid, filter_vec = build_gaussian_pyramid(im, max_levels, filter_size)
     new_im = im - expand(gaussian_pyramid[0 + 1], filter_vec)
     for i in range(len(gaussian_pyramid)):
@@ -216,31 +227,68 @@ def build_laplacian_pyramid(im, max_levels, filter_size):
 
 
 # ******************* 3.2 Laplacian pyramid reconstruction *******************
-  def laplacian_to_image(lpyr, filter_vec, coeff):
+def laplacian_to_image(lpyr, filter_vec, coeff):
+    """
+    the reconstruction of an image from its Laplacian Pyramid.
 
-      """
-      the reconstruction of an image from its Laplacian Pyramid
+    Before reconstructing the image img you should multiply each level i of
+    the laplacian pyramid by its corresponding coefficient coeff[i]. only
+    when this list is all ones we get the original image (up to a negligible
+    floating error, e.g. maximal absolute difference around 10−12).
+    When some values are different than 1 we will get filtering effects
 
-      :param lpyr: the Laplacian pyramid that is generated by the second
-      function in 3.1.
-      :param filter_vec: the filter that is generated by the second
-      function in 3.1.
-      :param coeff: a python list. The list length is the same as the number of
-       levels in the pyramid lpyr.
-      Before reconstructing the image img you should multiply each level i of
-      the laplacian pyramid by its corresponding coefficient coeff[i]. only
-      when this list is all ones we get the original image (up to a negligible
-      floating error, e.g. maximal absolute difference around 10−12).
-      When some values are different than 1 we will get filtering effects
-      :return:
-      """
+    :param lpyr: the Laplacian pyramid that is generated by the second
+    function in 3.1.
+    :param filter_vec: the filter that is generated by the second
+    function in 3.1.
+    :param coeff: a python list. The list length is the same as the number of
+     levels in the pyramid lpyr.
+    :return: img
+    """
+    img = coeff[-1] * lpyr[-1]
+    # for i = len(lpyr) - 2, len(lpyr) - 3, ..., 0.
+    for i in range(len(lpyr) - 2, -1, -1):
+        img = expand(img, filter_vec)
+        img += coeff[i] * lpyr[i]
+    return img
 
-if __name__ == '__main__':
-    monkey = "ex1_presubmit/presubmit_externals/monkey.jpg"
-    # plt.imshow(der_im, cmap='gray')
-    # plt.show()
-    # pyr, filter_vec = build_gaussian_pyramid(im, max_levels, filter_size)
-    # pyr, filter_vec = build_laplacian_pyramid(im, max_levels, filter_size)
+
+# ************************** 3.3 Pyramid display *****************************
+
+
+def render_pyramid(pyr, levels):
+    """
+    You should stretch the values of each pyramid level to [0, 1] before
+    composing it into the black wide image. Note that you should stretch the
+    values of both pyramid types: Gaussian and Laplacian
+
+    :param pyr: either a Gaussian or Laplacian pyramid as defined above.
+    :param levels: the number of levels to present in the result ≤ max_levels.
+    The number of levels includes the original image.
+    :return: a single black image in which the pyramid levels of the given
+    pyramid pyr are stacked horizontally (after stretching the values to
+    [0, 1]). The function render_pyramid should only return the big image res.
+    """
+    res = None  # np.asarray(pyr[0]) todo: can be none?
+    for i in range(levels):
+        # stretch the values of each pyramid level to [0, 1]
+
+        # composing it into the black wide image
+        res = np.hstack((res, np.asarray(pyr[i])))
+    return res
+
+
+def display_pyramid(pyr, levels):
+    """
+    The function display_pyramid should use render_pyramid to internally render
+    and then display the stacked pyramid image using plt.imshow().
+
+    :param pyr: see render_pyramid
+    :param levels: see render_pyramid
+    :return:
+    """
+    result = render_pyramid(pyr, levels)
+    plt.imshow(result)  # cmap='gray'
 
 
 def read_image(filename, representation):
